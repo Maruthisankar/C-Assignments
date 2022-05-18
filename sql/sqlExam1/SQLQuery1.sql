b3bf58b599c30f6b02112dde3efe6cb557d24a45
Maruthi Challa <m.challa@zensar.com>
2022-05-18 09:24:07 +0530

    sql sqlExam1

---
+++ sql/sqlExam1/SQLQuery1.sql
@@ -0,0 +1,14 @@
+create database Exam2
+use Exam2
+create table Emp8(
+Empid int not null,
+Name varchar(55),
+salary int,
+Deptno int)
+insert into Emp8 values(1,'martuthi',5000,10)
+insert into Emp8 values(2, 'sankar',12000,20)
+insert into Emp8 values(3, 'sai',7000,30)
+
+select * from Emp8
+
+
