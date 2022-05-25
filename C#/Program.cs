229dfb09df18b15ddb28dd3aaee582ae8eb603d2
Maruthisankar <105190709+Maruthisankar@users.noreply.github.com>
2022-05-25 09:05:47 +0530

    c#assignment6

--- C#/Program.cs
+++ C#/Program.cs
@@ -4,89 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment5
+namespace Assignment6
 {
-    public class WithdrawnException : ApplicationException
+    interface IStudent
     {
-        public WithdrawnException(string msg) : base(msg)
-        {
+        void showdetails(int studentid, string studentName);
+    }
 
+    class DayScholar : IStudent
+    {
+        public void showdetails(int studentid, string studentName)
+        {
+            Console.WriteLine($"The student details is studentid is {studentid}, student name is {studentName}", studentid, studentName);
         }
     }
-    public class Bank
+
+    class Resident : IStudent
     {
-        float Amount = 10000.00f;
-        float Balance;
-        string Acc_Name;
-        string Acc_Number;
-
-        public Bank()
+        public void showdetails(int studentid, string studentName)
         {
-            Console.WriteLine("Enter the Account Number : ");
-            Acc_Number = Console.ReadLine();
-            Console.WriteLine("Enter the Account holder Name :");
-            Acc_Name = Console.ReadLine();
-
-            Console.WriteLine($"The Account details is : AccountNum {Acc_Number}, Acount Namer {Acc_Name}, Amount is {Amount}");
-
-        }
-
-
-        public void DepositeAmount()
-        {
-            Console.WriteLine("Enter the Deposited Amount : ");
-            float DepAmount = Convert.ToSingle(Console.ReadLine());
-
-            if (DepAmount == 0)
-            {
-                Console.WriteLine("Deposite the minimum amount above 1000.00 ");
-
-            }
-            else
-            {
-                Balance = Amount + DepAmount;
-                Console.WriteLine("Your Account Balance is After amount deposite : " + Balance);
-            }
-        }
-
-        public void Amount_Withdrawn()
-        {
-
-            Console.WriteLine("Enter the WithDrawn Amount : ");
-            float Withdrawn_Amount = Convert.ToSingle(Console.ReadLine());
-
-
-            if (Withdrawn_Amount > Balance)
-            {
-                throw (new WithdrawnException("Insufficent balance"));
-            }
-            else
-            {
-                Balance = Balance - Withdrawn_Amount;
-                Console.WriteLine("Your Account Balance is After With Draw the amount :" + Balance);
-            }
+            Console.WriteLine($"The student details is studentid is {studentid}, student name is {studentName}", studentid, studentName);
         }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Bank bank = new Bank();
-            try
-            {
-                bank.DepositeAmount();
-                bank.Amount_Withdrawn();
+            Console.WriteLine("*****Dayscholar Details****");
+            IStudent ds = new DayScholar();
+            ds.showdetails(6745, "Mahesh");
 
-            }
-            catch (WithdrawnException we)
-            {
-                Console.WriteLine(we.Message);
-            }
-            catch (FormatException fe)
-            {
-                Console.WriteLine($"Enter only numbers ", fe);
-            }
-            Console.Read();
+            Console.WriteLine("*****Resident Details****");
+            IStudent re = new Resident();
+            re.showdetails(2748, "Krishna");
+
+            Console.ReadLine();
         }
     }
 }
